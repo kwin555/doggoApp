f52dab75eea7b4e2bc17d37e63599769a59d4802
kwin555 <ktn555@att.net>
2018-02-22 21:39:35 +0000

    added bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,7 @@
 //
 //= require rails-ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root 'doggo#index'
+  root 'doggos#index'
 end
